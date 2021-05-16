a389c4667cc55269c2356a98a5a983368c1e3683
Ivan <ivan.poganiuko@gmail.com>
2021-05-16 23:50:33 +0300

    feat: default image

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -41,6 +41,25 @@ const mapApiRatingToRate = (apiRating: any): IRate => {
     };
 }
 const mapApiProductToStuff = (apiProduct: any): IStuff => {
+    const getStringHash = (str: string): number => {
+        let hash = 0, i, chr;
+        if (str.length === 0)
+            return hash;
+
+        for (i = 0; i < str.length; i++) {
+            chr   = str.charCodeAt(i);
+            hash  = ((hash << 5) - hash) + chr;
+            hash |= 0;
+        }
+        return hash;
+    }
+
+    const urls = [
+        "https://uk.ellas-cookies.com/images/novosti-i-obshestvo/indeks-fizicheskogo-obema-produkcii_7.jpg",
+        "https://static.tildacdn.com/tild3266-6631-4638-a432-366265343835/Depositphotos_527161.jpg",
+        "https://yt3.ggpht.com/ytc/AAUvwnj9EohwA2kUdniHithcEGL1XaS4dPJd7PcFmtTcbg=s900-c-k-c0x00ffffff-no-rj"
+    ];
+
     return {
         id: apiProduct.id,
         name: apiProduct.title,
@@ -50,7 +69,7 @@ const mapApiProductToStuff = (apiProduct: any): IStuff => {
             mapApiCategoryToCategory(apiCategory)),
         rates: apiProduct.rating.map((apiRating: any) =>
             mapApiRatingToRate(apiRating)),
-        image: ""
+        image: apiProduct.image ?? urls[getStringHash(apiProduct.id) % urls.length]
     };
 }
 const mapApiProductItemToBasketStuff = (apiStuff: any): IBasketStuff => {
@@ -102,7 +121,6 @@ export const signin = async (user: ISigninUser) => {
     });
 
     return mapApiSigninResponseToLoggedInUser(response.data);
-    //return new VerifiedUser(response.data.name, response.data.accessToken);
 }
 export const getBasket = async (user: ILoggedInUser): Promise<IBasket> => {
     const response = await axios.get(`${ACCOUNT}/${user.id}`, {
